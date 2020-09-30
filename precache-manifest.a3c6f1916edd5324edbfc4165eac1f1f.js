e9e39018604abc054163850e5ac19f44c882b338
Manisha Arora <manishaarora@expedia.com>
2020-09-30 15:17:42 +0530

    Updates

---
+++ precache-manifest.a3c6f1916edd5324edbfc4165eac1f1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d11b3057f3ddf79c4af590b694c516",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5c185562d734042e07c6",
+    "url": "/github.io/monsters-rolodex/static/css/main.87afe3a6.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/github.io/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c185562d734042e07c6",
+    "url": "/github.io/monsters-rolodex/static/js/main.91370765.chunk.js"
+  },
+  {
+    "revision": "e8b598e4c6a9e02b2564",
+    "url": "/github.io/monsters-rolodex/static/js/runtime-main.89be258a.js"
+  }
+]);
